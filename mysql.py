6a3ccc140ce1c0fe5c1dbbf1297f198d3390d50f
Cxd <1400622711@qq.com>
2019-09-17 10:44:18 +0800

    add dict

---
+++ mysql.py
@@ -0,0 +1,25 @@
+'''
+ 操作数据库基本流程
+'''
+
+import pymysql
+
+# 连接数据库
+db = pymysql.connect(host='localhost',port=3306,user='root',password='123456',
+                     database='stu',charset='utf8')
+
+# 获取游标(操作数据库 执行sql语句 得到执行结果)
+cur = db.cursor()
+
+# 执行语句
+sql='insert into interest values(1,"Emma","draw","C","1420.00","xxx");'
+cur.execute(sql)
+
+# 提交到数据库
+db.commit()
+
+# 关闭游标
+cur.close()
+
+# 关闭数据库
+db.close()
